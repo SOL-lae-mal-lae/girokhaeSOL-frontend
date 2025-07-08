747c5bc34f824cd0febc90fa7cd15d80f9bc8228
PangHae <yds05074@gmail.com>
2025-07-08 17:47:03 +0900

    chore: add lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.plugins('import', 'prettier'),
-	...compat.extends('plugin:prettier/recommended', 'plugin:import/recommended'),
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	...compat.plugins('import', 'prettier'),
+	...compat.extends(
+		'plugin:prettier/recommended',
+		'plugin:import/recommended'
+	),
 	{
 		ignores: ['node_modules/', '.next/'],
 	},
@@ -74,6 +77,7 @@ const eslintConfig = [
 			],
 			'import/no-unresolved': 'error',
 			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'error',
 		},
 	},
 ];
