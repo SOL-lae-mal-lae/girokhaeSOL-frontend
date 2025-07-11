512231328da851931ad104598687061ce4deca36
PangHae <yds05074@gmail.com>
2025-07-11 14:08:54 +0900

    fix: 로그인된 유저가 있다면 메인화면으로 redirect

--- src/containers/(auth)/auth-required/AuthRequireContainer.tsx
+++ src/containers/(auth)/auth-required/AuthRequireContainer.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 import { SignInButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
 
 import { Button } from '@/components/ui/button';
 
-const AuthRequireContainer = () => {
+const AuthRequireContainer = async () => {
+	const user = await currentUser();
+
+	if (user) {
+		redirect('/');
+	}
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen gap-6 p-4">
 			<div className="text-center space-y-4">
