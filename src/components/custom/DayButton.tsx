1d7b4d3ccace3faf9ecc1d91b03cdc1b1abdec17
PangHae <yds05074@gmail.com>
2025-07-16 23:46:44 +0900

    feat: 달력 주말 비활성화

--- src/components/custom/DayButton.tsx
+++ src/components/custom/DayButton.tsx
@@ -28,6 +28,7 @@ const CustomDayButton = ({
 	const today = new Date();
 	today.setHours(0, 0, 0, 0);
 	const isFuture = day.date > today;
+	const isWeekend = day.date.getDay() === 0 || day.date.getDay() === 6;
 	// 오늘 날짜 판별
 	const isToday = day.date.getTime() === today.getTime();
 
@@ -50,12 +51,12 @@ const CustomDayButton = ({
 			data-range-start={modifiers.range_start}
 			data-range-end={modifiers.range_end}
 			data-range-middle={modifiers.range_middle}
-			disabled={isFuture}
+			disabled={isFuture || isWeekend}
 			className={cn(
 				'data-[selected-single=true]:bg-primary data-[selected-single=true]:text-primary-foreground data-[range-middle=true]:bg-accent data-[range-middle=true]:text-accent-foreground data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-ring/50 flex aspect-square size-auto w-full min-w-(--cell-size) flex-col gap-1 leading-none font-normal group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:ring-[3px] data-[range-end=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-start=true]:rounded-l-md [&>span]:text-xs [&>span]:opacity-70 relative',
 				defaultClassNames.day,
 				className,
-				isFuture &&
+				(isFuture || isWeekend) &&
 					'bg-gray-100/90 pointer-events-none select-none cursor-default hover:bg-gray-100/90 focus:bg-gray-100/90 focus:ring-0',
 				isToday &&
 					!isFuture &&
