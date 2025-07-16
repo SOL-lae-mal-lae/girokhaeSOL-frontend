b5da413e4ca34440b9d4ef2b437bdfb730d18f9d
hanuuuuU <han980004@naver.com>
2025-07-17 08:26:28 +0900

    feat: show financial statement

--- src/containers/trade-logs/new/NewTradeLogContainer.client.tsx
+++ src/containers/trade-logs/new/NewTradeLogContainer.client.tsx
@@ -1,14 +1,32 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { Plus, X } from 'lucide-react';
+import Image from 'next/image';
 
+import {
+	ChartColumn,
+	DollarSign,
+	Percent,
+	Plus,
+	TrendingUp,
+	X,
+} from 'lucide-react';
+
+import { FinancialStatementData } from '@/@types/financialStatement';
+import { LargeCard } from '@/components/cards/FinanceInfo/LargeCard/Index';
+import { SmallCard } from '@/components/cards/FinanceInfo/SmallCard/Index';
 import InfoCard from '@/components/cards/Info';
 import { AccountSelectDialog } from '@/components/dialogs';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
+} from '@/components/ui/sheet';
 import {
 	Table,
 	TableHeader,
@@ -18,6 +36,7 @@ import {
 	TableCell,
 } from '@/components/ui/table';
 import { Textarea } from '@/components/ui/textarea';
+import { getFinancialStatements } from '@/services/financial-statements';
 
 interface Props {
 	date: string;
@@ -93,6 +112,66 @@ export default function NewTradeLogContainerClient({ date }: Props) {
 	const handleChangeUrl = (idx: number, value: string) =>
 		setNewsUrls((prev) => prev.map((url, i) => (i === idx ? value : url)));
 
+	// 재무제표 sheet에 필요한 거
+	const [sheetOpen, setSheetOpen] = useState(false);
+	const [selectedCode, setSelectedCode] = useState('');
+	const [statements, setStatements] = useState<FinancialStatementData | null>(
+		null
+	);
+
+	// 종목 코드 눌렀을 때, 나올 재무제표 데이터
+	useEffect(() => {
+		if (sheetOpen && selectedCode) {
+			(async () => {
+				const data = await getFinancialStatements(selectedCode);
+				console.log(data);
+				setStatements(data); // 받아온 데이터 저장
+			})();
+		} else {
+			setStatements(null); // Sheet 닫힐 때 데이터 초기화
+		}
+	}, [sheetOpen, selectedCode]);
+
+	async function getFinanceData(code: string) {
+		setSelectedCode(code);
+		setSheetOpen(true);
+	}
+
+	// 단위 정리용~
+	function formatCurrency(value: bigint) {
+		const numValue = Number(value);
+
+		if (numValue >= 10_000) {
+			// 1조원 이상
+			const trillion = (numValue / 10_000).toFixed(1);
+			return `${trillion}조원`;
+		} else if (numValue >= 1) {
+			// 1억원 이상 1조원 미만
+			const billion = Math.floor(numValue);
+			return `${billion}억원`;
+		} else if (numValue >= 0.0001) {
+			// 1만원 이상 1억원 미만
+			const tenThousand = Math.floor(numValue / 0.0001);
+			return `${tenThousand}만원`;
+		} else {
+			// 1만원 미만
+			return `${numValue.toLocaleString()}원`;
+		}
+	}
+
+	// 이익률 계산기~
+	function calculateOperatingMargin(
+		revenue: bigint,
+		operatingIncome: bigint
+	) {
+		if (!revenue || !operatingIncome || Number(operatingIncome) === 0) {
+			return '0.00%';
+		}
+
+		const ratio = (Number(operatingIncome) / Number(revenue)) * 100;
+		return `${ratio.toFixed(2)}%`;
+	}
+
 	return (
 		<div className="w-full max-w-7xl mx-auto px-8 flex flex-col gap-8">
 			{/* 최상단 Title & 날짜 */}
@@ -163,7 +242,16 @@ export default function NewTradeLogContainerClient({ date }: Props) {
 								<TableBody>
 									{detailRows.map((row, i) => (
 										<TableRow key={i}>
-											<TableCell>{row.code}</TableCell>
+											<TableCell
+												onClick={(e) =>
+													getFinanceData(
+														e.currentTarget
+															.innerText
+													)
+												}
+											>
+												{row.code}
+											</TableCell>
 											<TableCell>{row.name}</TableCell>
 											<TableCell>
 												{row.buyAvg.toLocaleString()}
@@ -210,6 +298,120 @@ export default function NewTradeLogContainerClient({ date }: Props) {
 						</CardContent>
 					</Card>
 				</div>
+				{/* 재무제표 띄우기 */}
+				<Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+					<SheetContent className="!w-[30vw] flex flex-col !max-w-none pl-3 pr-3">
+						<SheetHeader>
+							<SheetTitle>
+								{statements
+									? `재무제표 - ${
+											statements.stock_code || '코드 없음'
+										}`
+									: '재무제표 정보 없음'}
+							</SheetTitle>
+						</SheetHeader>
+						{statements ? (
+							<div className="">
+								{/* pbr, per, 부채비율, EPS */}
+								<div className="grid grid-flow-row gap-4 grid-cols-2 ">
+									<SmallCard
+										title="PBR"
+										value={
+											statements.pbr
+												? statements.pbr
+												: '-'
+										}
+										color="blue"
+										icon={<TrendingUp />}
+										unit=""
+									/>
+									<SmallCard
+										title="PER"
+										value={
+											statements.per
+												? statements.per
+												: '-'
+										}
+										color="green"
+										icon={<ChartColumn />}
+										unit=""
+									/>
+									<SmallCard
+										title="부채비율"
+										value={
+											statements.debt_ratio
+												? statements.debt_ratio
+												: '-'
+										}
+										color="orange"
+										icon={<Percent />}
+										unit="%"
+									/>
+									<SmallCard
+										title="EPS"
+										value={
+											statements.eps
+												? statements.eps
+												: '-'
+										}
+										color="purple"
+										icon={<DollarSign />}
+										unit="원"
+									/>
+								</div>
+								<div className="flex flex-col">
+									<LargeCard
+										title="매출"
+										value={formatCurrency(
+											statements.revenue
+										)}
+										icon={<DollarSign size={15} />}
+										subtitle="연간 매출액"
+										color="green"
+									/>
+									<LargeCard
+										title="영업이익"
+										value={formatCurrency(
+											statements.operating_income
+										)}
+										icon={<TrendingUp size={15} />}
+										subtitle="영업 이익률"
+										color="blue"
+										subValue={`${calculateOperatingMargin(
+											statements.revenue,
+											statements.operating_income
+										)}`}
+									/>
+									<LargeCard
+										title="순이익"
+										value={formatCurrency(
+											statements.net_income
+										)}
+										icon={<ChartColumn size={15} />}
+										subtitle="순이익률"
+										color="purple"
+										subValue={`${calculateOperatingMargin(
+											statements.revenue,
+											statements.net_income
+										)}`}
+									/>
+								</div>
+							</div>
+						) : (
+							// 재무제표 데이터 없을 때
+							<div className="flex flex-col items-center justify-center h-full w-full">
+								<Image
+									src="/images/SOL_search.png"
+									alt="sol_search"
+									width={200}
+									height={200}
+								/>
+								<span>데이터가 존재하지 않습니다</span>
+							</div>
+						)}
+					</SheetContent>
+				</Sheet>
+
 				{/* 우측: 작성 폼 */}
 				<div className="flex-[1] min-w-[320px] max-w-[360px] flex flex-col gap-8 sticky top-8 h-fit self-start">
 					<Card>
