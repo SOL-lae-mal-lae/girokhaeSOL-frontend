f0e0ab31195f8586e08264ea3ec02277e56e18a5
PangHae <yds05074@gmail.com>
2025-07-21 13:16:51 +0900

    refactor: 데이터랩 nav bar 수정 및 컴포넌트 삭제

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -2,7 +2,7 @@ export const NAV_ITEMS = [
 	{ href: '/trade-logs', label: '매매일지' },
 	{ href: '/account', label: '계좌 등록' },
 	{ href: '/community', label: '커뮤니티' },
-	{ href: '/data-lab', label: '데이터랩' },
+	// { href: '/data-lab', label: '데이터랩' },
 	{ href: '/my-page', label: '마이페이지' },
 ];
 
