02bb7b4f5b11f6d36b9a8f86637fb9b09ea7e4b9
김혜윤 <gpdbs9409@naver.com>
2025-07-22 11:35:27 +0900

    feat: 마이페이지 레이아웃 및 더미 데이터 추가

---
+++ src/app/my-page/layout.tsx
@@ -0,0 +1,13 @@
+import CommunityInfoContainer from '@/containers/my-page/CommunityInfo';
+import UserInfoContainer from '@/containers/my-page/UserInfo';
+
+export default function MyPageLayout() {
+	return (
+		<div className="w-full max-w-7xl mx-auto px-8 flex flex-col gap-8 mt-8">
+			{/* 1. 유저 정보 */}
+			<UserInfoContainer />
+			{/* 2. 커뮤니티 활동 내역 */}
+			<CommunityInfoContainer />
+		</div>
+	);
+}
