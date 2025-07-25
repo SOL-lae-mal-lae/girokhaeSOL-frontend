459adffd4a5d24816a0322ad13d59739f7d11f8c
PangHae <yds05074@gmail.com>
2025-07-25 17:31:40 +0900

    fix: remove data-lab folder

--- src/app/data-lab/layout.tsx
+++
@@ -1,9 +0,0 @@
-// layout.tsx
-export default function MyPageLayout({ children }) {
-	return (
-		<div className="w-full max-w-7xl mx-auto px-8 flex flex-col gap-8 mt-8">
-			{/* 공통 레이아웃(예: 배경, wrapper 등) */}
-			{children}
-		</div>
-	);
-}
